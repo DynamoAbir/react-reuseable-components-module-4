f99298ae7b1a73811ae482a71125d275a04dab28
DynamoAbir <mhabir10@gmail.com>
2024-01-15 03:11:17 +0600

    adding nothing tbh

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h1 className="h-screen bg-red-500">SideBar</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
